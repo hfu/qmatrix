1b885617e840f9225fb1d10461fa60f107c85009
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2018-01-12 12:48:33 -0500

    Update qmatrix.rb

--- qmatrix.rb
+++ qmatrix.rb
@@ -1,3 +1,5 @@
+QMAX = 26
+
 class Fixnum
   def q
     0.step{|i|
@@ -6,16 +8,28 @@ class Fixnum
   end
 end
 
+def show(r, c)
+  print "** count #{c} ***\n"
+  QMAX.times {|q|
+    print ([q] + r[q]).map{|v| sprintf("%4d", v)}.join(' '), "\n"
+  }
+end
+
 def qmatrix(dir, maxzoom)
+  r = Array.new(QMAX).map{Array.new(maxzoom, 0)} # r[q][z]
+  c = 0
   0.upto(maxzoom) {|z|
     (2 ** z).times {|x|
       (2 ** z).times {|y|
+	c += 1
         path = "#{dir}/#{z}/#{x}/#{y}.mvt"
 	size = File.exist?(path) ? File.size(path) : 0
-	print "#{path} #{size.q}\n"
+	r[z][size.q] += 1
+	show(r, c) if c % 1000 == 0
       }
     }
   }
+  show(r, c)
 end
 
 if ARGV.size != 2
